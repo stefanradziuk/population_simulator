e6597ffe26160fa368d65780213583747e1926db
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2020-03-18 19:56:18 +0000

    migrated to arcade, basic infection simulation implemented, todo: bouncing, recovery, graphs

--- simulate.py
+++ simulate.py
@@ -1,17 +1,22 @@
 import random
 from math import pi, sin, cos, sqrt, pow
 import time
-import pyglet
-from pyglet.gl import *
+import arcade
 
 # some constants to play with
 
-step_size = 0.1
-step_delay = 10
-simulation_length = 100
-person_radius = 0.2
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Population Simulator"
 
-window = pyglet.window.Window()
+HEALTHY_COLOR = arcade.color.AMAZON
+INFECTED_COLOR = arcade.color.CARMINE_RED
+
+STEP_SIZE = 2
+STEP_DELAY = 2             # ms
+SIMULATION_LENGTH = 100
+PERSON_RADIUS = 1
+POPULATION_LENGTH = 10
 
 class Person:
     def __init__(self, coords):
@@ -22,27 +27,16 @@ class Person:
     def infect(self):
         self.healthy = False
 
+    def is_infected(self):
+        return not self.healthy
+
     def step(self):
-        new_x = self.coords[0] + step_size * sin(self.direction)
-        new_y = self.coords[1] + step_size * cos(self.direction)
+        new_x = (self.coords[0] + STEP_SIZE * sin(self.direction)) % SCREEN_WIDTH
+        new_y = (self.coords[1] + STEP_SIZE * cos(self.direction)) % SCREEN_HEIGHT
         self.coords = (new_x, new_y)
 
     def draw(self):
-        x = self.coords[0]
-        y = self.coords[1]
-
-        iterations = int(2 * person_radius * pi)
-        s = sin(2 * pi / iterations)
-        c = cos(2 * pi / iterations)
-    
-        dx, dy = person_radius, 0
-    
-        glBegin(GL_TRIANGLE_FAN)
-        glVertex2f(x, y)
-        for i in range(iterations+1):
-            glVertex2f(x + dx, y + dy)
-            dx, dy = (dx * c - dy * s), (dy * c + dx * s)
-        glEnd()
+        arcade.draw_circle_filled(self.coords[0], self.coords[1], PERSON_RADIUS * 10, HEALTHY_COLOR if self.healthy else INFECTED_COLOR)
 
 
     @staticmethod
@@ -50,39 +44,59 @@ class Person:
         return sqrt(pow(person1.coords[0] - person2.coords[0], 2)
                   + pow(person1.coords[1] - person2.coords[1], 2))
 
+    @staticmethod
+    def collide(person1, person2):
+        return Person.distance(person1, person2) <= PERSON_RADIUS * 16
+
 class Simulation:
     def __init__(self, size_x, size_y):
-        self.step_label = pyglet.text.Label(text="Simulation has not yet begun")
+        print("Simulation has not yet begun")
         self.collisions = 0
+        self.step = 0
 
         self.population = set()
         for i in range(size_x):
             for j in range(size_y):
-                self.population.add(Person((i, j)))
+                new_person = Person((i * SCREEN_WIDTH / POPULATION_LENGTH, j * SCREEN_HEIGHT / POPULATION_LENGTH))
+                if i == POPULATION_LENGTH / 2 and j == POPULATION_LENGTH / 2:
+                    new_person.infect()
+                self.population.add(new_person)
 
-    def run(self):
-        for step in range(simulation_length):
-            print("Simulation step #%d" % step)
-            self.step_label = pyglet.text.Label(text="Simulation step #%d" % step)
+    def runStep(self):
+            print("Simulation step #%d" % self.step)
             step_collisions = 0
             for person in self.population:
                 person.step()
-                person.draw()
                 for other_person in self.population:
-                    if (not person is other_person) and Person.distance(person, other_person) < 2 * person_radius:
+                    if (not person is other_person) and Person.collide(person, other_person):
                         step_collisions += 1
+                        if person.is_infected() or other_person.is_infected():
+                            person.infect()
+                            other_person.infect()
             print("  collisions: %d" % step_collisions)
-            collisions += step_collisions
-            time.sleep(step_delay / 1000)
+            self.collisions += step_collisions
+            self.step += 1
+            time.sleep(STEP_DELAY / 1000)
+
+    def end(self):
         print("%d total collisions" % collisions)
 
-simulation = Simulation(10, 10)
+class SimulationWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.simulation = Simulation(POPULATION_LENGTH, POPULATION_LENGTH)
 
-@window.event
-def on_draw():
-    window.clear()
-    simulation.run()
-    simulation.step_label.draw()
+    def on_draw(self):
+        arcade.start_render()
+        for person in self.simulation.population:
+            person.draw()
 
+    def on_update(self, delta_time):
+        self.simulation.runStep()
 
-pyglet.app.run()
+def main():
+    SimulationWindow()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
